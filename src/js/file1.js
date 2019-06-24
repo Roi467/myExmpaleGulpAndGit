94f47d18f7e3d2b5aa7359e2dbc372d362b84b1e
Roi Israeli <roi467@gmail.com>
2019-06-24 14:44:28 +0300

    first commit

---
+++ src/js/file1.js
@@ -0,0 +1,13 @@
+// Console 1
+function log(text)
+{
+  console.log(text);
+}
+
+log('Roi');
+
+
+
+// Console 2
+
+log('Test gulp');
---
+++ src/js/file2.js
@@ -0,0 +1,16 @@
+// Console 1
+function log(text)
+{
+  console.log(text);
+}
+
+log('File 2');
+
+
+
+log('Test watchs');
+
+
+// Console 2
+
+log('Test gulp');
